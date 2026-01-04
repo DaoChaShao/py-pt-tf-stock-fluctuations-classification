052e4f200d21a47b5d3b414c4adb60fea95dd832
DaoChaShao <27882272@qq.com>
2026-01-05 00:42:14 +0800

    feat: enhance data preprocessing with stratified column conversion and improved logging

--- pipeline/preprocessor.py
+++ pipeline/preprocessor.py
@@ -6,43 +6,61 @@
 # @File     :   preprocessor.py
 # @Desc     :   
 
-from datasets import load_dataset, DatasetDict
+from datasets import load_dataset, DatasetDict, ClassLabel
 from pathlib import Path
 
 from src.configs.cfg_hf import HF_CONFIG
 from src.utils.TF import (load_ds_dict_locally, summary_ds_dict,
-                          split_ds_dict)
+                          StratifiedColConverter,
+                          split_ds_dict, check_label_distribution)
+from src.utils.helper import Timer
+from src.utils.highlighter import lines
 
 
 def preprocess_data() -> DatasetDict:
     """ Preprocess data """
-    path: Path = Path(HF_CONFIG.FILE_PATHS.DATA_HF)
+    with Timer("Preprocessing data"):
+        path: Path = Path(HF_CONFIG.FILE_PATHS.DATA_HF)
 
-    if path.exists():
-        print("HuggingFace configuration file already exists")
+        if path.exists():
+            print("HuggingFace configuration file already exists")
+            lines()
 
-        # Access Hugging Face Dataset
-        dataset_dict = load_ds_dict_locally(path, file_type="arrow")
-        print(dataset_dict)
-        # Check the dataset details
-        # summary_dataset_dict(dataset_dict, "train", show_dup=False)
+            # Access Hugging Face Dataset
+            dataset_dict = load_ds_dict_locally(path, file_type="arrow")
+            print(dataset_dict)
+            # Check the dataset details
+            # summary_dataset_dict(dataset_dict, "train", show_dup=False)
+            lines()
 
-        # Split the data
-        ds_dict = split_ds_dict(dataset_dict)
-        print(ds_dict)
-        # summary_ds_dict(ds_dict, "train", show_dup=False)
-        # summary_ds_dict(ds_dict, "valid", show_dup=False)
-        # summary_ds_dict(ds_dict, "test", show_dup=False)
+            # Split the data
+            col: str = "PriceVariation"
+            converter = StratifiedColConverter(dataset_dict["train"], col)
+            dataset_dict["train"] = converter.fit()
+            # print(dataset_dict["train"].features)
+            # print(dataset_dict["train"].features[col].int2str(0))
+            ds_dict = split_ds_dict(dataset_dict, stratified_column=col)
+            print(ds_dict)
+            lines()
+            # summary_ds_dict(ds_dict, "train", show_dup=False)
+            # summary_ds_dict(ds_dict, "valid", show_dup=False)
+            # summary_ds_dict(ds_dict, "test", show_dup=False)
+            print(ds_dict["train"].features)
+            print(ds_dict["valid"].features)
+            print(ds_dict["test"].features)
+            lines()
 
-        return ds_dict
+            check_label_distribution(ds_dict, col)
 
-    else:
-        print(f"{path.name} doesn't exist!")
+            return ds_dict
 
-        ds_dict = load_dataset(HF_CONFIG.PARAMETERS.DATASET)
-        print(ds_dict)
+        else:
+            print(f"{path.name} doesn't exist!")
 
-        return ds_dict
+            ds_dict = load_dataset(HF_CONFIG.PARAMETERS.DATASET)
+            print(ds_dict)
+
+            return ds_dict
 
 
 if __name__ == "__main__":
